5db12ab5053a68d7b1b571aca52c53ef327b8252
ffoysal <miqbal@espial.com>
2019-06-07 08:47:39 -0400

    add lg

--- terraform/ecs/logs.tf
+++ terraform/ecs/logs.tf
@@ -6,11 +6,3 @@ resource "aws_cloudwatch_log_group" "app_log_group" {
     Name = "${var.deploy_name}"
   }
 }
-
-/*
-resource "aws_cloudwatch_log_stream" "app_log_stream" {
-  name           = "${var.deploy_name}-app-log-stream"
-  log_group_name = "${aws_cloudwatch_log_group.app_log_group.name}"
-}
-*/
-
